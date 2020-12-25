d2b0433425080693576f0e34c9c0364f15b48537
yanwei <1041203139@qq.com>
2020-12-25 13:36:17 +0800

    adjust

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,10 +1,30 @@
 <template>
   <div class="login-container">
-    <img src="../assets/image/bill-bg.png" alt="" class="bill-img">
-    <div class="wrapper" >
-      <a-input placeholder="请输入手机号" class="ani" swiper-animate-effect="lightSpeedIn" swiper-animate-duration="1s" swiper-animate-delay="0.3s"></a-input>
-      <a-input placeholder="请输入身份证后六位" class="ani" swiper-animate-effect="lightSpeedIn" swiper-animate-duration="1s" swiper-animate-delay="0.7s"></a-input>
-      <div @click="handleClick" class="check ani" swiper-animate-effect="swing" swiper-animate-duration="1s" swiper-animate-delay="1.6s">查看我的年度账单</div>
+    <img src="../assets/image/bill-bg.png" alt="" class="bill-img" />
+    <div class="wrapper">
+      <a-input
+        placeholder="请输入手机号"
+        class="ani"
+        swiper-animate-effect="lightSpeedIn"
+        swiper-animate-duration="1s"
+        swiper-animate-delay="0.3s"
+      ></a-input>
+      <a-input
+        placeholder="请输入身份证后六位"
+        class="ani"
+        swiper-animate-effect="lightSpeedIn"
+        swiper-animate-duration="1s"
+        swiper-animate-delay="0.7s"
+      ></a-input>
+      <div
+        @click="handleClick"
+        class="check ani"
+        swiper-animate-effect="swing"
+        swiper-animate-duration="1s"
+        swiper-animate-delay="1.6s"
+      >
+        查看我的年度账单
+      </div>
     </div>
   </div>
 </template>
@@ -20,8 +40,8 @@ export default {
     return {};
   },
   methods: {
-    handleClick(){
-      this.$emit('next')
+    handleClick() {
+      this.$emit("next");
     }
   }
 };
@@ -32,13 +52,13 @@ export default {
   height: 100%;
   width: 100%;
   padding: 120px 0 80px 0;
-  background: url('../assets/image/first-page.jpg') center/cover no-repeat;
+  background: url("../assets/image/first-page.jpg") center/cover no-repeat;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   .bill-img {
     width: 100%;
-    margin-top: calc(48 / 375 * 100vw)
+    margin-top: calc(48 / 375 * 100vw);
   }
   .wrapper {
     width: 250px;
@@ -49,13 +69,13 @@ export default {
     }
   }
   .check {
-    width: 100%   ;
+    width: 100%;
     margin: 0 auto;
     background-color: red;
     font-size: 18px;
     padding: 8px 16px;
     border-radius: 10px;
-    color:#fff;
+    color: #fff;
     font-weight: 600;
     letter-spacing: 3px;
     margin-top: 36px;
